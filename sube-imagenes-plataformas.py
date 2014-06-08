68c6ca939e593cd6910261a6460b8f145beb770b
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2014-06-08 10:31:27 +0200

    sube imagenes de plataformas a partir de su twitter

---
+++ sube-imagenes-plataformas.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 emijrp
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import catlib
+import os
+import pagegenerators
+import re
+import urllib
+import wikipedia
+
+def main():
+    site = wikipedia.Site('15mpedia', '15mpedia')
+    cat = catlib.Category(site, u"Category:Plataformas")
+    gen = pagegenerators.CategorizedPageGenerator(cat)
+    pre = pagegenerators.PreloadingGenerator(gen, pageNumber=60)
+    
+    for page in pre:
+        wtitle = page.title()
+        wtext = page.get()
+        
+        if not re.search(ur"(?im)\{\{\s*Infobox Plataforma", wtext):
+            continue
+        
+        print '\n===', wtitle, '==='
+        newtext = wtext
+                
+        #imagen
+        if not re.search(ur"(?im)\|\s*imagen\s*=", newtext):
+            twitter = re.findall(ur"(?im)\|\s*twitter\s*=([^\r\n]+)\r\n", newtext)
+            if twitter:
+                twitter = twitter[0].split(',')[0].strip()
+                f = urllib.urlopen("https://twitter.com/%s" % twitter)
+                html = unicode(f.read(), 'utf-8')
+                imageurl = re.findall(ur"data-resolved-url-large=\"(https://pbs.twimg.com/profile_images/[^\"]+)\"", html)
+                if imageurl:
+                    imageurl = imageurl[0]
+                    if 'default_profile' in imageurl:
+                        print 'Default twitter image, skiping'
+                        continue
+                    desc = u"{{Infobox Archivo\n|embebido id=\n|embebido usuario=\n|embebido título=\n|descripción=Logotipo de [[%s]].\n|fuente={{twitter|%s}}\n}}" % (wtitle, twitter)
+                    if imageurl.endswith('jpeg') or imageurl.endswith('jpg'):
+                        ext = 'jpg'
+                    elif imageurl.endswith('pneg') or imageurl.endswith('png'):
+                        ext = 'png'
+                    else:
+                        print 'Twitter image extension is %s, skiping' % (imageurl.split('.')[-1])
+                        continue
+                    imagename = u"%s.%s" % (re.sub(u'"', u'', wtitle), ext)
+                    #https://www.mediawiki.org/wiki/Manual:Pywikibot/upload.py
+                    os.system('python upload.py -lang:15mpedia -family:15mpedia -keep -filename:"%s" -noverify "%s" "%s"' % (imagename.encode('utf-8'), imageurl.encode('utf-8'), desc.encode('utf-8')))
+                    newtext = re.sub(ur"(?im)\{\{Infobox Plataforma", ur"{{Infobox Plataforma\n|imagen=%s" % (imagename), newtext)
+                    wikipedia.showDiff(wtext, newtext)
+                    page.put(newtext, u"BOT - Añadiendo imagen")
+        
+if __name__ == '__main__':
+    main()
