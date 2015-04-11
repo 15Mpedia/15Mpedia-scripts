2a9b11df08901fc7cfee5b239c3ada9d87a90eaa
user <emijrp@gmail.com>
2015-04-11 16:28:13 +0200

    fixing

--- 15mpedia_family.py
+++ 15mpedia_family.py
@@ -25,6 +25,62 @@ class Family(family.WikimediaFamily):
         self.langs = {
             '15mpedia': 'wiki.15m.cc',
         }
+        """
+        self.namespaces[4] = {
+            '_default': u'15Mpedia',
+        }
+        
+        self.namespaces[5] = {
+            '_default': u'15Mpedia discusión',
+        }
+        
+        self.namespaces[6] = {
+            '_default': u'Archivo',
+        }
+        
+        self.namespaces[7] = {
+            '_default': u'Archivo discusión',
+        }
+        
+        self.namespaces[106] = {
+            '_default': u'Formulario',
+        }
+        """
+        # Wikimedia wikis all use "bodyContent" as the id of the <div>
+        # element that contains the actual page content; change this for
+        # wikis that use something else (e.g., mozilla family)
+        self.content_id = "bodyContent"
 
-    def protocol(self, code):                                                         
+    def scriptpath(self, code):
+        """The prefix used to locate scripts on this wiki.
+
+        This is the value displayed when you enter {{SCRIPTPATH}} on a
+        wiki page (often displayed at [[Help:Variables]] if the wiki has
+        copied the master help page correctly).
+
+        The default value is the one used on Wikimedia Foundation wikis,
+        but needs to be overridden in the family file for any wiki that
+        uses a different value.
+
+        """
+        return '/w'
+
+    # Which version of MediaWiki is used? REQUIRED
+    def version(self, code):
+        # Replace with the actual version being run on your wiki
+        return '1.21alpha'
+
+    def code2encoding(self, code):
+        """Return the encoding for a specific language wiki"""
+        # Most wikis nowadays use UTF-8, but change this if yours uses
+        # a different encoding
+        return 'utf-8'
+    
+    def path(self, code):
+        return '/w/index.php'
+
+    def apipath(self, code):
+        return '/w/api.php'
+
+    def protocol(self, code):
         return 'HTTP'
--- user-config.py
+++ user-config.py
@@ -4,7 +4,3 @@ mylang = '15mpedia'
 family = '15mpedia'
 
 usernames['15mpedia']['15mpedia'] = u'BotQuincemayista'
-
-minthrottle = 1
-maxthrottle = 1
-put_throttle = 1
