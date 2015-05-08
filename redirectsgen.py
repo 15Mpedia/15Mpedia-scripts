80a009b40d0d466307d958fe7d12d8bbc3093f8f
user <emijrp@gmail.com>
2015-05-08 15:42:57 +0200

    adaptandolo al nuevo pywikibot

--- redirectsgen.py
+++ redirectsgen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2011-2014 emijrp
+# Copyright (C) 2011-2015 emijrp <emijrp@gmail.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -21,8 +21,8 @@
 import re
 import sys
 
-import wikipedia
-import pagegenerators
+import pywikibot
+import pywikibot.pagegenerators as pagegenerators
 import unicodedata
 
 def removeaccute(s):
@@ -43,7 +43,7 @@ def unquote(s):
 def main():
     skip = u''
     if len(sys.argv) > 1:
-        site = wikipedia.Site(sys.argv[1], sys.argv[1])
+        site = pywikibot.Site(sys.argv[1], sys.argv[1])
     else:
         print 'python script.py wikifamily [skiptopage]'
         sys.exit()
@@ -52,13 +52,21 @@ def main():
     gen = pagegenerators.AllpagesPageGenerator(start=skip, namespace=0, site=site)
     pre = pagegenerators.PreloadingGenerator(gen, pageNumber=250)
     alltitles = []
+    #c = 0
     for page in pre:
         if not page.exists(): #do not put .isRedirectPage() or it will never find redirects when checking below before creating
             continue
         alltitles.append(page.title())
         print page.title()
+        #c +=1
+        #if c > 250:
+        #    break
         
     for wtitle in alltitles:
+        if wtitle.startswith('Lista de votaciones') or \
+            wtitle.startswith('Votaciones'):
+            continue
+        
         if len(wtitle) > 1:
             wtitle_ = wtitle[0]+wtitle[1:].lower()
             redirects = set()
@@ -114,11 +122,12 @@ def main():
             for redirect in redirects:
                 redirect = redirect.strip()
                 if redirect and redirect != wtitle and not redirect in alltitles:
-                    red = wikipedia.Page(site, redirect)
+                    red = pywikibot.Page(site, redirect)
                     if not red.exists():
                         output = u"#REDIRECT [[%s]]" % (wtitle)
                         msg = u"BOT - Creating redirect to [[%s]]" % (wtitle)
-                        red.put(output, msg)
+                        red.text = output
+                        red.save(msg)
 
 if __name__ == '__main__':
     main()
