263c494c81e59217a5e7a2184187fbfc02a6f33e
emijrp <emijrp@gmail.com>
2015-07-01 22:37:15 +0200

    completando

--- crear-municipios.py
+++ crear-municipios.py
@@ -21,6 +21,11 @@ import re
 import sys
 import pywikibot
 
+def logerror(errorline):
+    error = open('crear-municipios.errores', 'a')
+    error.write(errorline.encode('utf-8'))
+    error.close()
+   
 def main():
     codsccaa = {'01': u'Andalucía', }
     codsprov = {'04': u'Provincia de Almería', }
@@ -82,23 +87,19 @@ def main():
                     (eswiki.exists() and eswiki.isDisambig()) or \
                     (eswiki.exists() and not re.search(ur'(?im)\{\{\s*Ficha de localidad de España', eswiki.text)):
                     print u'Municipio %s no encontrado en eswiki. Escribiendo al log de errores' % eswikititle
-                
-                    error = open('crear-municipios.errores', 'a')
-                    errorline = u'%s\n' % (municipio)
-                    error.write(errorline.encode('utf-8'))
-                    error.close()
+                    logerror(u'%s no encontrado en eswiki\n' % (municipio))
                     continue
 
         if eswiki.exists() and not eswiki.isRedirectPage() and not eswiki.isDisambig():
             if re.search(ur'(?im)\{\{\s*Ficha de localidad de España', eswiki.text):
                 escudo = u''
                 if re.search(ur'(?im)escudo\s*=', eswiki.text):
-                    escudo = re.findall(ur'escudo\s*=\s*([^\n\r\|\[\]]+)', eswiki.text)[0].strip()
+                    escudo = re.findall(ur'escudo\s*=\s*([^\n\r\|\[\]\{\}]+)', eswiki.text)[0].strip()
                     if escudo == 'no':
                         escudo = u''
                 bandera = u''
                 if re.search(ur'(?im)bandera\s*=', eswiki.text):
-                    bandera = re.findall(ur'bandera\s*=\s*([^\n\r\|\[\]]+)', eswiki.text)[0].strip()
+                    bandera = re.findall(ur'bandera\s*=\s*([^\n\r\|\[\]\{\}]+)', eswiki.text)[0].strip()
                     if bandera == 'no':
                         bandera = u''
                 comarca = u''
@@ -131,15 +132,53 @@ def main():
 * {{wikipedia|es|%s}}
 }}""" % (nombre, pais, codsccaa[codccaa], codccaa, codsprov[codprov], codprov, comarca, codmuni, escudo, bandera, deuda2010, web, eswikititle)
         
-        print infobox
-        
-        """page = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), u'%s' % (nombre))
+        page = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), u'%s' % (nombre))
         if page.exists():
-            print 'La pagina ya existe'
-            continue
+            if not re.search(ur'(?im)\{\{\s*Infobox Municipio', page.text):
+                print 'La pagina existe pero no tiene infobox municipio'
+                logerror(u'%s existe en 15Mpedia pero no tiene infobox\n' % (nombre))
+                continue
+            
+            print 'La pagina ya existe, rellenando lo que falta'
+            newtext = page.text
+            add = []
+            if not re.search(ur'(?im)\|nombre=', newtext):
+                add.append(u'|nombre=%s' % (nombre))
+            if not re.search(ur'(?im)\|país=', newtext):
+                add.append(u'|país=%s' % (pais))
+            if not re.search(ur'(?im)\|comunidad autónoma=', newtext):
+                add.append(u'|comunidad autónoma=%s' % (codsccaa[codccaa]))
+            if not re.search(ur'(?im)\|comunidad autónoma código=', newtext):
+                add.append(u'|comunidad autónoma código=%s' % (codccaa))
+            if not re.search(ur'(?im)\|provincia=', newtext):
+                add.append(u'|provincia=%s' % (codsprov[codprov]))
+            if not re.search(ur'(?im)\|provincia código=', newtext):
+                add.append(u'|provincia código=%s' % (codprov))
+            if not re.search(ur'(?im)\|comarca=', newtext):
+                add.append(u'|comarca=%s' % (comarca))
+            if not re.search(ur'(?im)\|municipio código=', newtext):
+                add.append(u'|municipio código=%s' % (codmuni))
+            if not re.search(ur'(?im)\|escudo=', newtext):
+                add.append(u'|escudo=%s' % (escudo))
+            if not re.search(ur'(?im)\|bandera=', newtext):
+                add.append(u'|bandera=%s' % (bandera))
+            if not re.search(ur'(?im)\|deuda viva=', newtext):
+                add.append(u'|deuda viva={{deuda viva|año=2010|euros=%s}}' % (deuda2010))
+            if not re.search(ur'(?im)\|sitio web=', newtext):
+                add.append(u'|sitio web=%s' % (web))
+            if not re.search(ur'(?im)\|enlaces externos=', newtext):
+                add.append(u'|enlaces externos={{enlaces externos}}\n* {{wikipedia|es|%s}}' % (eswikititle))
+            
+            if add:
+                newtext = newtext.replace(u'{{Infobox Municipio', u'{{Infobox Municipio\n%s' % ('\n'.join(add)))
+                pywikibot.showDiff(page.text, newtext)
+                page.save(u'BOT - Añadiendo datos de Wikipedia', botflag=False)
+            else:
+                print u'Nada que añadir'
         else:
+            print infobox
             page.text = infobox
-            page.save(u'BOT - Creando municipio', botflag=False)"""
+            page.save(u'BOT - Creando municipio usando Wikipedia', botflag=False)
         
         c += 1
 
