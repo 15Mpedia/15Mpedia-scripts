67aca00ea2f2744cf2b61adc82192f7a3f01fea1
emijrp <emijrp@gmail.com>
2015-11-22 11:11:56 +0100

    efemerides rss

---
+++ efemerides-fundacion.php
@@ -0,0 +1,3 @@
+<?php
+include('https://15mpedia.org/wiki/Especial:Ask/-5B-5BPage-20has-20default-20form::Centro-20social-7C-7CPlataforma-7C-7CRealojo-5D-5D-20-5B-5Bfundaci%C3%B3n-20string::~*-2F' + date('m') + '-2F' + date('d') + '-5D-5D/-3FFundaci%C3%B3n-23-2D/format%3Drss/limit%3D3/offset%3D0');
+?>
