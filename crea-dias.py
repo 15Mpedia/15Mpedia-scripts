db2e1f42f6f0492b31c0dd8b69790a414a99a06c
emijrp <emijrp@gmail.com>
2016-07-04 19:15:49 +0200

    crea dias redirecciones

--- crea-dias.py
+++ crea-dias.py
@@ -19,7 +19,7 @@ import pywikibot
 
 num2month = {1:'enero', 2:'febrero', 3:'marzo', 4:'abril', 5:'mayo', 6:'junio', 7:'julio', 8:'agosto', 9:'septiembre', 10:'octubre', 11:'noviembre', 12:'diciembre'}
 
-for year in [2016]:
+for year in [2017]:
     for month in range(1,13):
         for day in range(1, 32):
             if month == 2 and day > 28:
@@ -33,3 +33,21 @@ for year in [2016]:
                 output = u"{{Infobox Día\n|día=%d-%02d-%02d\n}}" % (year, month, day)
                 p.text = output
                 p.save(u"BOT - Creando día")
+            
+            redirects = [
+                '%d-%02d-%02d' % (year, month, day), 
+                '%02d-%02d-%d' % (day, month, year), 
+                '%d/%02d/%02d' % (year, month, day), 
+                '%02d/%02d/%d' % (day, month, year), 
+                '%d %02d %02d' % (year, month, day), 
+                '%02d %02d %d' % (day, month, year), 
+                '%d de %s de %d' % (day, num2month[month][0].upper()+num2month[month][1:], year), 
+                '%d %s %d' % (day, num2month[month], year), 
+                '%d %s %d' % (day, num2month[month][0].upper()+num2month[month][1:], year), 
+            ]
+            for red in redirects:
+                redp = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), red)
+                output = u"#REDIRECT [[%s]]" % (title)
+                redp.text = output
+                redp.save(u"BOT - Creando redirección")
+
