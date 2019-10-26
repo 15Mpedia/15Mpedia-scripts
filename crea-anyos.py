c8ae0d9a340a0589356073a5fb4d61deb6d2dc4d
emijrp <emijrp@gmail.com>
2019-10-26 18:02:22 +0200

    .

--- crea-anyos.py
+++ crea-anyos.py
@@ -15,10 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
 import pywikibot
 
 def main():
-    for year in range(1780, 1996):
+    overwrite = True
+    currentyear = datetime.datetime.now().year
+    for year in range(1780, currentyear+1):
         #[[1950]]
         infobox = u"""{{Infobox Año
 |año=%s
@@ -35,26 +38,22 @@ def main():
             page.text = infobox
             page.save(u'BOT - Creando categoría de año', botflag=True)
         
-        #[[Categoría:Personas nacidas en 1950]]
-        infobox = u"""{{navegación por año y tema categoría|tema=Personas nacidas|año=%s}}
-{{main|%s#Nacimientos}}
-
-[[Categoría:Personas por año de nacimiento| %s]]""" % (year, year, year)
-        page = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), u'Categoría:Personas nacidas en %s' % (year))
-        if not page.exists():
-            page.text = infobox
-            page.save(u'BOT - Creando categoría de nacimientos por año', botflag=True)
-        
         #[[Categoría:Personas fallecidas en 1950]]
-        infobox = u"""{{navegación por año y tema categoría|tema=Personas fallecidas|año=%s}}
-{{main|%s#Fallecimientos}}
-
-[[Categoría:Personas por año de fallecimiento| %s]]""" % (year, year, year)
+        infobox = u'{{Categoría personas fallecidas por año|año=%s}}' % (year)
         page = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), u'Categoría:Personas fallecidas en %s' % (year))
-        if not page.exists():
+        if not page.exists() or overwrite:
             page.text = infobox
             page.save(u'BOT - Creando categoría de fallecimientos por año', botflag=True)
         
+        if year >= 2000:
+            continue
+        
+        #[[Categoría:Personas nacidas en 1950]]
+        infobox = u'{{Categoría personas nacidas por año|año=%s}}' % (year)
+        page = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), u'Categoría:Personas nacidas en %s' % (year))
+        if not page.exists() or overwrite:
+            page.text = infobox
+            page.save(u'BOT - Creando categoría de nacimientos por año', botflag=True)
 
 if __name__ == '__main__':
     main()
