d206d3c91534052c0882aee7f5ad3b79145e265c
emijrp <emijrp@gmail.com>
2020-05-05 17:23:14 +0200

    .

--- municipios-redirects-prov.py
+++ municipios-redirects-prov.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2018 emijrp <emijrp@gmail.com>
+# Copyright (C) 2018-2020 emijrp <emijrp@gmail.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -22,16 +22,19 @@ import pywikibot
 import pywikibot.pagegenerators as pagegenerators
 import re
 
-def getURL(url=''):
-    raw = ''
-    req = urllib.request.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
-    try:
-        raw = urllib.request.urlopen(req).read().strip().decode('utf-8')
-    except:
-        pass
-    return raw
-
 def main():
+    provincias = {
+        'A Coruña': ['A Coruña', 'La Coruña'], 
+        'La Coruña': ['La Coruña', 'A Coruña'], 
+        'Ourense': ['Ourense', 'Orense'], 
+        'Orense': ['Orense', 'Ourense'], 
+        'Araba': ['Araba', 'Álava'], 
+        'Álava': ['Álava', 'Araba'], 
+        'Gipuzkoa': ['Gipuzkoa', 'Guipúzcoa'], 
+        'Guipúzcoa': ['Guipúzcoa', 'Gipuzkoa'], 
+        'Bizkaia': ['Bizkaia', 'Vizcaya'], 
+        'Vizcaya': ['Vizcaya', 'Bizkaia'], 
+    }
     site = pywikibot.Site('15mpedia', '15mpedia')
     catnames = [
         'Categoría:Municipios de España', 
@@ -50,6 +53,8 @@ def main():
             wtext = page.text
             wtitle = page.title()
             
+            print('\n== %s ==' % (wtitle))
+            
             if skip:
                 if skip == wtitle:
                     skip = ''
@@ -63,17 +68,19 @@ def main():
             
             if not re.search(r'{{Infobox Municipio', wtext):
                 continue
-            print('\n== %s ==' % (wtitle))
+            
             m = re.findall(r"(?im)\|provincia=Provincia de ([^\n\|]+)", wtext)
             if not m:
                 print("No tiene provincia")
                 continue
-            provincia = m[0]
-            redtitle = "%s (%s)" % (wtitle, provincia)
-            redpage = pywikibot.Page(site, redtitle)
-            if not redpage.exists():
-                redpage.text = "#redirect [[%s]]" % (wtitle)
-                redpage.save("BOT - Creando redirección a [[%s]]" % (wtitle), botflag=True)
+            prov = m[0]
+            provinciasaliases = prov in provincias.keys() and provincias[prov] or [prov]
+            for provincia in provinciasaliases:
+                redtitle = "%s (%s)" % (wtitle, provincia)
+                redpage = pywikibot.Page(site, redtitle)
+                if not redpage.exists():
+                    redpage.text = "#redirect [[%s]]" % (wtitle)
+                    redpage.save("BOT - Creando redirección a [[%s]]" % (wtitle), botflag=True)
 
 if __name__ == '__main__':
     main()
