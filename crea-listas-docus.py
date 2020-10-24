fd568f8a34920ef50af5caabf478de3004b96a9a
emijrp <emijrp@gmail.com>
2020-10-24 14:37:54 +0200

    .

--- crea-listas-docus.py
+++ crea-listas-docus.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2015 emijrp <emijrp@gmail.com>
+# Copyright (C) 2015-2020 emijrp <emijrp@gmail.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -17,40 +17,80 @@
 
 import pywikibot
 
-for year in range(1930, 2020):
-    title = u'Lista de documentales de %d' % (year)
-    p = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), title)
-    output = u"{{Lista de documentales por año|año=%s}}" % (year)
+site = pywikibot.Site('15mpedia', '15mpedia')
+for year in range(1930, 2021):
+    title = 'Cine de %d' % (year)
+    p = pywikibot.Page(site, title)
+    output = "{{Cine por año|año=%d}}" % (year)
     p.text = output
-    p.save(u"BOT - Creando lista")
+    p.save("BOT - Creando página")
+    
+    """title = u'Lista de documentales de %d' % (year)
+    p = pywikibot.Page(site, title)
+    output = u"{{Lista de documentales por año|año=%d}}" % (year)
+    p.text = output
+    p.save("BOT - Creando lista")
     
     title = u'Lista de documentales en %d' % (year)
-    p = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), title)
-    output = u"#redirect [[Lista de documentales de %s]]" % (year)
+    p = pywikibot.Page(site, title)
+    output = u"#redirect [[Lista de documentales de %d]]" % (year)
     p.text = output
-    p.save(u"BOT - Creando redirección")
+    p.save("BOT - Creando redirección")
     
     title = u'Categoría:Documentales de %d' % (year)
-    p = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), title)
-    output = u"{{Categoría documentales por año|año=%s}}" % (year)
+    p = pywikibot.Page(site, title)
+    output = u"{{Categoría documentales por año|año=%d}}" % (year)
     p.text = output
-    p.save(u"BOT - Creando categoría")
+    p.save("BOT - Creando categoría")
 
     title = u'Lista de películas de %d' % (year)
-    p = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), title)
-    output = u"{{Lista de películas por año|año=%s}}" % (year)
+    p = pywikibot.Page(site, title)
+    output = u"{{Lista de películas por año|año=%d}}" % (year)
     p.text = output
-    p.save(u"BOT - Creando lista")
+    p.save("BOT - Creando lista")
     
     title = u'Lista de películas en %d' % (year)
-    p = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), title)
-    output = u"#redirect [[Lista de películas de %s]]" % (year)
+    p = pywikibot.Page(site, title)
+    output = u"#redirect [[Lista de películas de %d]]" % (year)
     p.text = output
-    p.save(u"BOT - Creando redirección")
+    p.save("BOT - Creando redirección")
     
     title = u'Categoría:Películas de %d' % (year)
-    p = pywikibot.Page(pywikibot.Site('15mpedia', '15mpedia'), title)
-    output = u"{{Categoría películas por año|año=%s}}" % (year)
+    p = pywikibot.Page(site, title)
+    output = u"{{Categoría películas por año|año=%d}}" % (year)
     p.text = output
-    p.save(u"BOT - Creando categoría")
+    p.save("BOT - Creando categoría")"""
 
+for year in range(1848, 2021):
+    title = 'Literatura de %d' % (year)
+    p = pywikibot.Page(site, title)
+    output = "{{Literatura por año|año=%d}}" % (year)
+    p.text = output
+    p.save("BOT - Creando página")
+
+"""
+    title = u'Lista de libros de %d' % (year)
+    p = pywikibot.Page(site, title)
+    output = u"{{Lista de libros por año|año=%d}}" % (year)
+    p.text = output
+    p.save("BOT - Creando lista")
+    
+    title = u'Lista de libros en %d' % (year)
+    p = pywikibot.Page(site, title)
+    output = u"#redirect [[Lista de libros de %d]]" % (year)
+    p.text = output
+    p.save("BOT - Creando redirección")
+    
+    title = u'Categoría:Libros de %d' % (year)
+    p = pywikibot.Page(site, title)
+    output = u"{{Categoría libros por año|año=%d}}" % (year)
+    p.text = output
+    p.save("BOT - Creando categoría")
+"""
+
+for year in range(1780, 2021):
+    title = 'Plantilla:%d' % (year)
+    p = pywikibot.Page(site, title)
+    output = "{{Navbox Año|año=%d}}<noinclude>[[Categoría:Plantillas de navegación]]</noinclude>" % (year)
+    p.text = output
+    p.save("BOT - Creando plantilla")
